071a2ebff3ce45fb39410599abb148cf9393abcc
sibhelly <sibhelly@MacBook-Pro-Nikita.local>
2025-09-18 15:19:15 +0300

    feat: add models for auth

---
+++ internal/models/entity/session.go
@@ -0,0 +1,38 @@
+package entity
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+// session model for jwt auth
+type Session struct {
+	gorm.Model
+	UserID       uint      `gorm:"not null;index" json:"user_id"`
+	UUID         uuid.UUID `gorm:"type:uuid;not null;unique_index" json:"uuid"`
+	RefreshToken string    `gorm:"size:255" json:"-"`
+	ExpiresAt    time.Time `gorm:"type:timestamptz" json:"expires_at"`
+	UserAgent    string    `gorm:"size:255" json:"user_agent"`
+	LastIP       string    `gorm:"size:255" json:"last_ip"`
+	User         User      `gorm:"foreignKey:UserID" json:"user,omitempty"`
+}
+
+// creating uuid for unique session
+func (s *Session) BeforeCreate(tx *gorm.DB) error {
+	if s.UUID == uuid.Nil {
+		s.UUID = uuid.New()
+	}
+	return nil
+}
+
+// check expired session
+func (s *Session) IsExpired() bool {
+	return time.Now().After(s.ExpiresAt)
+}
+
+// check valid of session
+func (s *Session) IsValid() bool {
+	return !s.IsExpired() && s.RefreshToken != ""
+}
---
+++ internal/models/entity/user.go
@@ -0,0 +1,17 @@
+package entity
+
+import "gorm.io/gorm"
+
+// model of user
+type User struct {
+	gorm.Model
+	Name     string    `gorm:"size:50;not null" json:"name"`
+	Surname  string    `gorm:"size:50;not null" json:"surname"`
+	Tgname   string    `gorm:"type:varchar(50);unique_index;not null" json:"tg_username"`
+	Password string    `gorm:"size:255;not null" json:"-"`
+	Sessions []Session `gorm:"foreignKey:UserID" json:"sessions,omitempty"`
+}
+
+func (User) TableName() string {
+	return "users"
+}
